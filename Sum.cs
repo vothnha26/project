c40711d3f8b2ef51b4c7a67334d64b837fda942a
vothnha26 <vothanhnha26@gmail.com>
2024-07-13 02:45:34 +0700

    update/Tong-func

--- Sum.cs
+++ Sum.cs
@@ -10,6 +10,7 @@ namespace Program
         }
         static void Main(string[] args)
         {
+            //tong
             int a, b;
             a = Convert.ToInt32(Console.ReadLine());
             b = Convert.ToInt32(Console.ReadLine());
